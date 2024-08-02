680f3a9c402cf85be5fcd67a006e6be3baa3a81d
Sephixum <emotive.5353@gmail.com>
2024-08-02 14:27:41 +0330

    Add precompiled headers and core

---
+++ src/core/core.hpp
@@ -0,0 +1,7 @@
+#ifndef __CORE_HPP
+#define __CORE_HPP
+
+#include "defines.hpp"
+#include "logger.hpp"
+
+#endif // __CORE_HPP
---
+++ src/core/pch.hpp
@@ -0,0 +1,28 @@
+#ifndef __PCH_HPP
+#define __PCH_HPP
+
+#if __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#if __cplusplus
+}
+#endif // __cplusplus
+
+#if __cplusplus
+#include <algorithm>
+#include <cstdint>
+#include <filesystem>
+#include <format>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <variant>
+#include <vector>
+
+#endif // __cplusplus
+
+#endif // __PCH_HPP
